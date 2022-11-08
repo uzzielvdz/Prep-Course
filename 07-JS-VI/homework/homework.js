55039d2e957557cd36a6ca73ab375707da798d96
GRACIELA ORBE <uzzieloww@outlook.com>
2022-11-08 13:57:42 -0600

    Tarea 6

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,20 +1,27 @@
 // Do not change any of the function names
 
+const { arrayContiene } = require("../../04-JS-III/homework/homework");
+
 function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+return nombre[0].toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+
+  return cb(n1,n2);
+
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +29,21 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+
+var SumaTotal = numeros.reduce(function(acc,curr) {
+  return acc + curr;
+}, 0);
+
+  cb(SumaTotal);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+ for (var i = 0; i < array.length; i++) {
+  cb(array[i]);
+  }
 }
 
 function map(array, cb) {
@@ -35,12 +51,25 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+
+  NuevoArray = array.map(function(el) {
+    return cb(el);
+  });
+
+  return NuevoArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+var NuevoArray =[];
+for (var i = 0; i < array.length; i++) {
+  if (array[i][0] === "a") {
+    NuevoArray.push(array[i]);
+  }
+}
+  return NuevoArray;
 }
 
 // No modificar nada debajo de esta línea
